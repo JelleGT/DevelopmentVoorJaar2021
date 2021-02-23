26a62816191ec87c85c6c9eb2a7af9f8ced01352
Jelle Tiepel <414532@student.fontys.nl>
2021-02-23 16:03:32 +0100

    DEV Challenge intro vierkanten
    Er is een nieuw deel aan de scene toegevoegd waarin de vierkant naar het midden toekrimpen

--- intro.js
+++ intro.js
@@ -1,7 +1,13 @@
 //Er worden 8 cirkels aangemaakt die proportioneel steeds iets groter worden
-for(x = 2; x < 10; x++) {
+for(x = 2; x < 9; x++) {
     var div = document.createElement('div');
     div.className = 'circle one';
     div.style.cssText = 'width:' + x*x*0.12*75 + 'px;height:' + x*x*0.12*75 + 'px;z-index:-' + x + ';animation-delay:' + x*0.25 + 's;';
     document.body.appendChild(div);
   }
+for (y = 5; y < 15; y++) {
+  var sqr = document.createElement('div');
+  sqr.className = "circle three";
+  sqr.style.cssText = 'width:' + y*y*0.18*50 + 'px;height:' + y*y*0.18*50 + 'px;';
+  document.body.appendChild(sqr);
+}
