d7f5bb162f9fdeaa724a5fecee913204b42ca8cd
Jelle Tiepel <414532@student.fontys.nl>
2021-03-01 14:12:13 +0100

    DEV Challenge Flying Prototype Shooting
    Het schip kan nu objecten schieten die naar boven het scherm bewegen en daarna verdwijnen

--- fly.js
+++ fly.js
@@ -3,4 +3,15 @@ const onMouseMove = (e) =>{
   ship.style.left = e.pageX + 'px';
   ship.style.top = e.pageY + 'px';
 }
+const shoot = (e) =>{
+  var div = document.createElement('div');
+  div.setAttribute("id","bullet");
+  div.style.cssText = 'width:5px;height:10px;background-color:blue;left:' + e.pageX + 'px;top:' + e.pageY + 'px;';
+  document.body.appendChild(div);
+  setTimeout(function(){
+    var gone = document.getElementById('bullet');
+    gone.remove();
+  }, 1500);
+}
 document.addEventListener('mousemove', onMouseMove);
+document.addEventListener("mousedown", shoot)
