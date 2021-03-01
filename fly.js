373a1b09fe5766c118ad6df164117a3dda1684b9
Jelle Tiepel <414532@student.fontys.nl>
2021-03-01 14:47:52 +0100

    DEV Challenge Flying explosie
    Na een tijdje explodeert het schip met geluid en er is ook alvast audio toegevoegd.

--- fly.js
+++ fly.js
@@ -1,8 +1,11 @@
+//Voor het bewegen van het schip
 let ship = document.getElementById('gunship');
 const onMouseMove = (e) =>{
   ship.style.left = e.pageX + 'px';
   ship.style.top = e.pageY + 'px';
 }
+
+//Voor het schieten van bullets
 const shoot = (e) =>{
   var div = document.createElement('div');
   div.setAttribute("id","bullet");
@@ -15,3 +18,14 @@ const shoot = (e) =>{
 }
 document.addEventListener('mousemove', onMouseMove);
 document.addEventListener("mousedown", shoot)
+
+//Na een tijdje explodeert het schip met geluid
+setTimeout(function(){
+  var explosion = document.getElementById("gunship");
+  setTimeout(function(){
+    gunship.style.display = "none";
+  }, 1200);
+  var boom = document.getElementById("boom");
+  boom.play();
+  explosion.src = "FlyingAssets/Explosion.gif"
+}, 6000);
