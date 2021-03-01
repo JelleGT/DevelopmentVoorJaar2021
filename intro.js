b7417352bcdaecacf64ea48b15fdbe382ef1d7d4
Jelle Tiepel <414532@student.fontys.nl>
2021-03-01 09:57:26 +0100

    DEV Challenge intro prototype
    De eerste variant van de intro staat er nu. Door de verschillende objecten ontstaat er wel wat lag, voor de volgende iteratie zal dit verandert worden.

--- intro.js
+++ intro.js
@@ -5,9 +5,15 @@ for(x = 2; x < 9; x++) {
     div.style.cssText = 'width:' + x*x*0.12*75 + 'px;height:' + x*x*0.12*75 + 'px;z-index:-' + x + ';animation-delay:' + x*0.25 + 's;';
     document.body.appendChild(div);
   }
-for (y = 5; y < 15; y++) {
+for (y = 3; y < 12; y++) {
   var sqr = document.createElement('div');
   sqr.className = "circle three";
-  sqr.style.cssText = 'width:' + y*y*0.18*50 + 'px;height:' + y*y*0.18*50 + 'px;';
+  sqr.style.cssText = 'width:' + y*y*0.18*50 + 'px;height:' + y*y*0.18*50 + 'px;' + 'px;z-index:1;';
   document.body.appendChild(sqr);
+}
+for (z = 3; z < 12; z++) {
+  var cir = document.createElement('div');
+  cir.className = "circle four";
+  cir.style.cssText = 'width:' + z*z*0.2*50 + 'px;height:' + z*z*0.2*50 + 'px;' + 'px;z-index:2;';
+  document.body.appendChild(cir);
 }
