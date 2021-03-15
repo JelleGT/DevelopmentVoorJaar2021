74ac256dfff5ed08ff2d85770fcf6657696c2ede
Jelle Tiepel <414532@student.fontys.nl>
2021-03-15 15:14:41 +0100

    DEV Bootcamp API: Pokemon 2
    Je kan nu een pokemon opzoeken via het dexnummer

--- pokemon.js
+++ pokemon.js
@@ -1,27 +1,28 @@
-let apiData = {
-  url: "https://pokeapi.co/api/v2/",
-  type: "pokemon",
-  id: "389" //document.getElementById("number").innerHTML
-}
+function searchDex() {
 
-const apiUrl = `${apiData.url}${apiData.type}/${apiData.id}` ;
+  let apiData = {
+    url: "https://pokeapi.co/api/v2/",
+    type: "pokemon",
+    id: document.getElementById("dexNumber").value
+  }
 
-console.log(apiUrl);
+  const apiUrl = `${apiData.url}${apiData.type}/${apiData.id}` ;
 
-fetch(apiUrl)
-  .then( (data) => data.json())
-  .then( (pokemon) => makeHtml(pokemon) )
+  fetch(apiUrl)
+    .then( (data) => data.json())
+    .then( (pokemon) => makeHtml(pokemon) )
 
-const makeHtml = (data) => {
-  console.log(data)
-  const html = `
-    <div class="name">${data.name}</div>
-    <img src=${data.sprites.front_default}
-    <div class="details">
-      <span>Height: ${data.height}</span>
-      <span>Weight: ${data.weight}</span>
-    </div>
-  `
-  const pokemonDiv = document.querySelector(".pokemon");
-  pokemonDiv.innerHTML = html;
+  const makeHtml = (data) => {
+    const html = `
+      <div class="name">${data.name}</div>
+      <img src=${data.sprites.front_default}
+      <div class="details">
+        <span>Height: ${data.height}</span>
+        <span>Weight: ${data.weight}</span>
+      </div>
+    `
+    const pokemonDiv = document.querySelector(".pokemon");
+    pokemonDiv.innerHTML = html;
+  }
+  console.log("hello")
 }
